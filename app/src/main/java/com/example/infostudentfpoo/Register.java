5179957999e4c9bfa96362e32df4b49b9d0cd85a
SebasAngarita <SebasAngarita@users.noreply.github.com>
2020-06-01 22:37:10 -0500

    CAMBIO LOGIN

--- app/src/main/java/com/example/infostudentfpoo/Register.java
+++ app/src/main/java/com/example/infostudentfpoo/Register.java
@@ -47,7 +47,7 @@ public class Register extends AppCompatActivity {
         mEmail      = findViewById(R.id.Email);
         mPassword   = findViewById(R.id.password);
         mPhone      = findViewById(R.id.phone);
-        mRegisterBtn= findViewById(R.id.registerBtn);
+        mRegisterBtn= findViewById(R.id.RegisterBtn);
         mLoginBtn   = findViewById(R.id.createText);
 
         fAuth = FirebaseAuth.getInstance();
